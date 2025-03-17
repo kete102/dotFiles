39d8ce401da325fd1b3be7f451489238cacebdfc
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-17 17:22:13 +0100

    some nvim config
    Better save, inlay hints and HSL support

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -16,10 +16,35 @@ map("n", "<leader>bo", function()
   Snacks.bufdelete.other()
 end, { desc = "Delete Other Buffers" })
 
+-- Inlay Hints
 map("n", "<leader>i", function()
   require("flavius.lsp").toggleInlayHints()
 end)
 
+-- HSL support
 map("n", "<leader>r", function()
   require("craftzdog.hsl").replaceHexWithHSL()
 end)
+
+-- Redefine Ctrl+s to save with the custom function
+vim.api.nvim_set_keymap("n", "<C-s>", ":lua SaveFile()<CR>", { noremap = true, silent = true })
+
+-- Custom save function
+function SaveFile()
+  -- Check if a buffer with a file is open
+  if vim.fn.empty(vim.fn.expand("%:t")) == 1 then
+    vim.notify("No file to save", vim.log.levels.WARN)
+    return
+  end
+
+  local filename = vim.fn.expand("%:t") -- Get only the filename
+  local success, err = pcall(function()
+    vim.cmd("silent! write") -- Try to save the file without showing the default message
+  end)
+
+  if success then
+    vim.notify(filename .. " Saved!") -- Show only the custom message if successful
+  else
+    vim.notify("Error: " .. err, vim.log.levels.ERROR) -- Show the error message if it fails
+  end
+end
